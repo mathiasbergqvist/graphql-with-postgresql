cb193f120f1d645b52518216bb7224525098bd68
Mathias <mathiasbergqvist88@gmail.com>
2020-04-24 13:33:43 +0200

    Mutations for adding and deleting messages

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { ApolloServer, gql } from 'apollo-server-express';
 import 'dotenv/config';
 import cors from 'cors';
+import uuidv4 from 'uuid/v4';
 
 const app = express();
 
@@ -14,6 +15,10 @@ const schema = gql`
         user(id: ID!): User
         messages: [Message!]!
         message(id: ID!): Message!
+    }
+    type Mutation {
+        createMessage(text: String!): Message!
+        deleteMessage(id: ID!): Boolean!
     }
     type User {
         id: ID!
@@ -71,6 +76,30 @@ const resolvers = {
         },
         message: (parent, { id }) => {
             return messages[id];
+        },
+    },
+    Mutation: {
+        createMessage: (parent, { text }, { me }) => {
+            const id = uuidv4();
+
+            const message = {
+                id,
+                text,
+                userId: me.id,
+            };
+
+            messages[id] = message;
+            users[me.id].messageIds.push(id);
+
+            return message;
+        },
+        deleteMessage: (parent, { id }) => {
+            const { [id]: message, ...otherMessages } = messages;
+            if (!message) {
+                return false;
+            }
+            messages = otherMessages;
+            return true;
         },
     },
     User: {
